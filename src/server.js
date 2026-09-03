7f05592976b87f8c9bd636674aae4e473279c687
Lais Vargas <lais_a_vargas@estudante.sesisenai.org.br>
2026-09-03 15:36:45 -0300

    terminei

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json("Seja bem vindo a minha API")
+})
+
+app.listen(port, () => {
+    console.log('Seja Bem vindo a minha API')
+})
