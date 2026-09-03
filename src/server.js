45680260fd29132a9fe14fee88ddb74e05ea8864
Lais Vargas <lais_a_vargas@estudante.sesisenai.org.br>
2026-09-03 16:53:09 -0300

    AGR

--- src/server.js
+++ src/server.js
@@ -1,14 +1,25 @@
 import express from 'express'
+import { pool } from './database/db.js'
 
 const app = express()
 const port = 3000
 
-app.use(express.json())
- 
+// Lê o json trafegado na aplicação
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.json("Seja bem vindo a minha API")
-})
+    res.json("Hello World")
+});
+
+app.get("/frutas", async (req, res) => {
+    try {
+        const response = await pool.query("SELECT * FROM frutas");
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 app.listen(port, () => {
-    console.log('Seja Bem vindo a minha API')
-})
+    console.log(`API rodando em: http://localhost:${port}`);
+});
